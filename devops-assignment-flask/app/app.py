ee453c3941a89ba90f8cc954c8614b034db4e1f8
Prakash Ayyamperumal <prakash.a051@Prakashs-MacBook-Pro.local>
2025-10-24 16:25:13 +0530

    Add CI/CD workflow

--- devops-assignment-flask/app/app.py
+++ devops-assignment-flask/app/app.py
@@ -1,14 +1,20 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route("/health")
 def health():
     return jsonify(status="healthy"), 200
 
+
 @app.route("/")
 def index():
     return jsonify(message="Sample Flask app is running"), 200
 
+
 if __name__ == "__main__":
+    # Default port matches Helm service targetPort
     app.run(host="0.0.0.0", port=8080)
+
